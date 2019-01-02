95826e6c0f10e9ea5757129a568febbc94df63e4
谢河源 <xieheyuan@zhimadi.cn>
2019-01-02 09:33:49 +0800

    下属详情

--- pages/mine/staff.js
+++ pages/mine/staff.js
@@ -14,35 +14,6 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
@@ -57,10 +28,9 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  seeDetail:function() {
+    wx.navigateTo({
+      url: './staffDetail',
+    })
   }
 })
--- pages/mine/staffDetail.js
+++ pages/mine/staffDetail.js
@@ -5,62 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cates: [{
+        name: '待完成'
+      },
+      {
+        name: '已完成'
+      }
+    ],
+    isShowFloatTab: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  onLoad: function(options) {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    * 生命周期函数--监听页面加载
+    */
+  onPageScroll: function (event) {
+    var scrollTop = event.scrollTop;
+    if (scrollTop >= floatTop && !this.data.isShowFloatTab) {
+      this.setData({
+        isShowFloatTab: true,
+      });
+    } else if (scrollTop < floatTop && this.data.isShowFloatTab) {
+      this.setData({
+        isShowFloatTab: false,
+      });
+    }
+  },
 
+  /**
+   * 获取滑动导致悬浮Tab出现的高度
+   */
+  getScrollTop: function() {
+    var that = this;
+    if (wx.canIUse('getSystemInfo.success.screenWidth')) {
+      wx: wx.getSystemInfo({
+        success: function (res) {
+          rate = res.screenWidth / 750;
+          floatTop = 84 * rate;
+          that.setData({
+            scrollTop: 84 * res.screenWidth / 750,
+            scrollHeight: res.screenHeight / (res.screenWidth / 750) - 128,
+          });
+        }
+      });
+    }
   }
 })
